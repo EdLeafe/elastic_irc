90eaad09d43fd80e56357d15be781b6f31884373
EdLeafe <ed@leafe.com>
2021-11-22 19:27:48 -0600

    Added some simple scripts for listing indexes and snaps.

---
+++ list_indexes.py
@@ -0,0 +1,11 @@
+import requests
+
+HQ = ".elastichq"
+url = "http://datahost:9200/_cat/indices"
+response = requests.request("GET", url)
+
+indexes = [ln.split()[2] for ln in response.text.splitlines()]
+# Remove the ElasticHQ index, if present
+if HQ in indexes:
+    indexes.remove(HQ)
+print(indexes)
---
+++ list_snaps.py
@@ -0,0 +1,15 @@
+import requests
+import pudb
+
+pudb.set_trace()
+
+HQ = ".elastichq"
+url = "http://datahost:9200/_snapshot/elasticback/_all"
+response = requests.request("GET", url)
+snaps = response.json()["snapshots"]
+print(f"Total Snapshots: {len(snaps)}")
+
+for snap in snaps[:-10:-1]:
+    print(f"Name: {snap['snapshot']}   {snap['end_time']}")
+    idxs = sorted([idx for idx in snap["indices"] if not idx.startswith(".")])
+    print(f"  Indexes: {', '.join(idxs)}")
