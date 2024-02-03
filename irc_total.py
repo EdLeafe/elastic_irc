0d7d663f2fac9b7005fac92abd8b757bfd4c3f97
EdLeafe <ed@leafe.com>
2024-02-03 17:02:47 -0600

    Added option for simple numeric output of the total.

--- irc_total.py
+++ irc_total.py
@@ -14,13 +14,17 @@ console = Console()
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
 @click.option("--chan", "-c", help="Only count records for the specified channel")
-def main(chan):
+@click.option("--numeric", "-n", is_flag=True, help="Just print the total with no additional text")
+def main(chan, numeric):
     if chan:
         body = {"query": {"term": {"channel": chan}}}
         r = es.count(index="irclog", body=body, params={"request_timeout": TIMEOUT})
     else:
         r = es.count(index="irclog", params={"request_timeout": TIMEOUT})
     total = r["count"]
+    if numeric:
+        print(total)
+        return
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     if chan:
         console.print(
