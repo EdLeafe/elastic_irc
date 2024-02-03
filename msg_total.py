751d4a615e01ecf8f9e934bdd6b01ffa7ede206d
EdLeafe <ed@leafe.com>
2024-02-03 22:52:07 +0000

    Updated to run on server
     Please enter the commit message for your changes. Lines starting

--- msg_total.py
+++ msg_total.py
@@ -1,18 +1,22 @@
 from datetime import datetime
 
 import click
+from rich.console import Console
 
 import utils
 
 es = utils.get_elastic_client()
+console = Console()
 
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
 def main():
     r = es.count(index="email")
     total = r["count"]
-    print(
-        f"There are {utils.format_number(total)} documents in the index at {datetime.now().strftime('%H:%M:%S')}."
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    console.print(
+        f"There are [magenta bold]{utils.format_number(total)}[/magenta bold] documents in the "
+        f"index at [yellow bold]{now}[/yellow bold]."
     )
 
 
